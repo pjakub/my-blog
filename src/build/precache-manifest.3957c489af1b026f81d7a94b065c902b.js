c30a283e561a961cbdf128126f119ce188832e17
pjakubowski <peter.m.jakubowski@gmail.com>
2020-12-30 11:38:52 -0500

    First commit

---
+++ src/build/precache-manifest.3957c489af1b026f81d7a94b065c902b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dca371d63dd54c4ad26669f514a6a6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7e2b943b7b7563a63d8",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "975a03303b1b22d4f186",
+    "url": "/static/js/2.989ffb9d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.989ffb9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e2b943b7b7563a63d8",
+    "url": "/static/js/main.666c84d7.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
